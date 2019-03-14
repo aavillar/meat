c8ec053c23e910048c31ede8bfecf52a3785d290
Ricardo Araujo Sena <ricardoaraujosena.sena@gmail.com>
2019-03-13 21:59:40 -0300

    Utilizando Zonas

--- src/app/app.error.handler.ts
+++ src/app/app.error.handler.ts
@@ -2,31 +2,33 @@ import { LoginService } from './security/login/login.service';
 import { NotificationService } from 'app/shared/messages/notification.service';
 import { HttpErrorResponse } from '@angular/common/http';
 import 'rxjs/add/observable/throw';
-import { ErrorHandler, Injectable, Injector } from '@angular/core';
+import { ErrorHandler, Injectable, Injector, NgZone } from '@angular/core';
 
 @Injectable()
 export class AplicationErrorHandler extends ErrorHandler {
     constructor(private ns: NotificationService,
-                private injector: Injector) {
+        private injector: Injector,
+        private zone: NgZone) {
         super();
     }
     handleError(errorResponse: HttpErrorResponse | any) {
         if (errorResponse instanceof HttpErrorResponse) {
-            const message = errorResponse.error.message;
-            switch (errorResponse.status) {
-                case 401:
-                this.injector.get(LoginService).handleLogin();
-                    break;
-                case 403:
-                this.ns.notify(message || 'Não autorizado');
-                    break;
-                case 404:
-                this.ns.notify(message || 'Recurso não encontrado');
-                    break;
-
-                default:
-                    break;
-            }
+            this.zone.run(() => {
+                const message = errorResponse.error.message;
+                switch (errorResponse.status) {
+                    case 401:
+                        this.injector.get(LoginService).handleLogin();
+                        break;
+                    case 403:
+                        this.ns.notify(message || 'Não autorizado');
+                        break;
+                    case 404:
+                        this.ns.notify(message || 'Recurso não encontrado');
+                        break;
+                    default:
+                        break;
+                }
+            })
         }
         super.handleError(errorResponse)
     }
